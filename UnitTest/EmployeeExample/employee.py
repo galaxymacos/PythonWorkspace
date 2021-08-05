89fa68fa3cc9dc80f3cd3f9008f6e3c1b43942f4
Xun Ruan <galaxymacos@gmail.com>
2021-08-05 13:35:44 +0800

    Make change to the structure of the directory

--- UnitTest/EmployeeExample/employee.py
+++ UnitTest/EmployeeExample/employee.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Employee:
     raise_amt = 1.05
     
@@ -9,10 +12,18 @@ class Employee:
     @property
     def email(self):
         return '{}.{}@email.com'.format(self.first, self.last)
-        
+
     @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
         
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
+
